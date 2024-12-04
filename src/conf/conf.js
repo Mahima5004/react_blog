2a4df004741670cffd7efa142d5ee9a696631c66
Mahima <mahimamishra5008@gmail.com>
2024-12-04 15:38:43 +0530

    create authentication service with appwrite

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,4 +1,5 @@
 const conf = {
+  appwriteUrl : String(process.env.REACT_APP_APPWRITE_PROJECT_URL),
   appwriteProjectId: String(process.env.REACT_APP_APPWRITE_PROJECT_ID),
   appwriteDatabaseId: String(process.env.REACT_APP_APPWRITE_DATABASE_ID),
   appwriteCollectionId: String(process.env.REACT_APP_APPWRITE_COLLECTION_ID),
